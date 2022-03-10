44787557c69b518eb06e6cca2b8fe2dc9831a840
slon2plus <9857752669@mail.ru>
2022-03-10 02:25:53 -0800

    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="wrap">
+      <div className="cart">
+        <div className="front">
+          <span>front</span>
+        </div>
+        <div className="back">
+          <span>back</span>
+        </div>
+      </div>
+    </div>
+  );
+}
